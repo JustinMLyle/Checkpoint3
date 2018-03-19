7df29662875ad399a56b799e0b840217d25b46bb
justinmlyle <justin.m.lyle@gmail.com>
2018-03-19 14:17:15 -0500

    MVC controller part

--- Checkpoint3/App_Start/RouteConfig.cs
+++ Checkpoint3/App_Start/RouteConfig.cs
@@ -18,6 +18,10 @@ namespace Checkpoint3
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{name}/{id}"
+            );
         }
     }
 }
